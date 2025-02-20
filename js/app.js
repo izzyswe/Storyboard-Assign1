324d468e4480fb05495e8163dc71900a2f672aeb
Isaac Kim <isaackim@Isaacs-MacBook-Air.local>
2025-02-20 10:46:28 -0500

    updated css/js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
+//creating a noun array
+let noun = [["dog", "cat", "bird", "fish", "rabbit", "hamster", "turtle", "lizard", "snake", "frog", "toad", "salamander", "iguana", "chameleon", "gecko", "crocodile", "alligator", "dinosaur", "dragon", "unicorn", "pegasus", "phoenix", "griffin", "hippogriff", "wyvern", "minotaur", "centaur", "mermaid", "siren", "kraken", "leviathan", "hydra", "chimera", "gryphon"], 
+            ["mother", "father", "brother, sister", "aunt", "uncle", "cousin", "grandmother", "grandfather", "niece", "nephew", "daughter", "son", "granddaughter", "grandson", "great-grandmother", "great-grandfather", "great-aunt", "great-uncle", "great-niece", "great-nephew", "great-granddaughter", "great-grandson", "stepmother", "stepfather", "stepbrother", "stepsister", "stepdaughter", "stepson", "step-grandmother", "step-grandfather", "step-granddaughter", "step-grandson", "step-great-grandmother", "step-great-grandfather", "step-great-aunt", "step-great-uncle", "step-great-niece", "step-great-nephew", "step-great-granddaughter", "step-great-grandson"], ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon", "mango", "nectarine", "orange", "pear", "quince", "raspberry", "strawberry", "tangerine", "ugli", "watermelon", "xigua", "yuzu", "zucchini"], ["car", "bus", "train", "plane", "boat", "submarine", "helicopter", "motorcycle", "bicycle", "scooter", "skateboard", "rollerblades", "carriage", "wagon", "rickshaw", "gondola", "cable car", "funicular", "tram", "trolley", "monorail", "maglev", "bullet train", "m engine", "diesel engine", "electrelectric car", "hybrid car", "solar-powered car", "wind-powered car", "water-powered car", "coal-powered car", "nuclear-powered car", "gas-powered car", "oil-powered car", "biofuel-powered car", "hydrogen-powered car"], ["
+
 
 function generateTable() {
     var table = document.getElementById("table");
 };
+
+
