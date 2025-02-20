5511a3d60746ab6899c3668c7be0598acb90a1a0
Isaac Kim <isaackim@Isaacs-MacBook-Air.local>
2025-02-20 11:10:58 -0500

    added the neccessary 2d arrays

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 //creating a noun array
-let noun = [["dog", "cat", "bird", "fish", "rabbit", "hamster", "turtle", "lizard", "snake", "frog", "toad", "salamander", "iguana", "chameleon", "gecko", "crocodile", "alligator", "dinosaur", "dragon", "unicorn", "pegasus", "phoenix", "griffin", "hippogriff", "wyvern", "minotaur", "centaur", "mermaid", "siren", "kraken", "leviathan", "hydra", "chimera", "gryphon"], 
-            ["mother", "father", "brother, sister", "aunt", "uncle", "cousin", "grandmother", "grandfather", "niece", "nephew", "daughter", "son", "granddaughter", "grandson", "great-grandmother", "great-grandfather", "great-aunt", "great-uncle", "great-niece", "great-nephew", "great-granddaughter", "great-grandson", "stepmother", "stepfather", "stepbrother", "stepsister", "stepdaughter", "stepson", "step-grandmother", "step-grandfather", "step-granddaughter", "step-grandson", "step-great-grandmother", "step-great-grandfather", "step-great-aunt", "step-great-uncle", "step-great-niece", "step-great-nephew", "step-great-granddaughter", "step-great-grandson"], ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon", "mango", "nectarine", "orange", "pear", "quince", "raspberry", "strawberry", "tangerine", "ugli", "watermelon", "xigua", "yuzu", "zucchini"], ["car", "bus", "train", "plane", "boat", "submarine", "helicopter", "motorcycle", "bicycle", "scooter", "skateboard", "rollerblades", "carriage", "wagon", "rickshaw", "gondola", "cable car", "funicular", "tram", "trolley", "monorail", "maglev", "bullet train", "m engine", "diesel engine", "electrelectric car", "hybrid car", "solar-powered car", "wind-powered car", "water-powered car", "coal-powered car", "nuclear-powered car", "gas-powered car", "oil-powered car", "biofuel-powered car", "hydrogen-powered car"], ["
+const noun = [
+            ["dog", "cat", "bird", "fish", "rabbit", "hamster", "turtle", "lizard", "snake", "frog", "toad", "salamander", "iguana", "chameleon", "gecko", "crocodile", "alligator", "dinosaur", "dragon", "unicorn", "pegasus", "phoenix", "mermaid"], 
+            ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon", "mango", "nectarine", "orange", "pear", "raspberry", "strawberry", "tangerine", "watermelon", "zucchini"],
+            ["car", "bus", "train", "plane", "boat", "submarine", "helicopter", "motorcycle", "bicycle", "scooter", "skateboard", "rollerblades", "carriage", "wagon", "trolley", "bullet train", "hybrid car", "solar-powered car"], 
+          ];
+//creating a verb array
+const verb = [
+            ["ran", "jumped", "hopped", "skipped", "walked", "crawled", "slithered", "swimmed", "flew", "soared", "glided", "floated", "sailed", "driven", "rode", "skated", "skiied", "snowboarded", "surfed", "dived", "parachuted", "bungee jumped", "skydived", "hang glided"],
+            ["cooked", "baked", "fried", "grilled", "boiled", "steamed", "sauteed", "stir-fried", "microwaved", "toasted", "roasted", "smoked", "cured", "pickled", "fermented", "canned", "jellied", "preserved", "dehydrated", "freeze-dried", "blanched", "poached", "scrambled", "scalded"],
+           ];
 
+//creating a adjective array
+const adjective = [
+                  ["a big", "a small", "a tiny", "a huge", "a gigantic", "a enormous", "a massive", "a miniscule", "a microscopic", "a colossal", "a immense", "a vast", "a boundless", "a limitless", "an infinite", "an eternal", "an endless", "a bottomless", "unfathomable", "an immeasurable", "an unlimited", "an unbounded", "an unrestricted", "an unconfined"],
+                  ["a red", "a orange", "a yellow", "a green", "a blue", "a purple", "a pink", "a brown", "a black", "a white", "a gray", "a silver", "a gold", "a bronze", "a copper", "a platinum", "a turquoise", "a teal", "a indigo", "a violet", "a magenta"],
+                  ["a hot", "a cold", "a warm", "a cool", "a freezing", "a boiling", "a scalding", "a chilled", "a frozen", "a icy", "a slushy", "a sizzling", "a steaming", "a blistering", "a sweltering", "a sweaty", "a muggy", "a humid", "a dry", "a moist", "a damp", "a wet", "a soggy", "a saturated"],
+                  ["a happy", "a sad", "an angry", "a mad", "a glad", "a joyful", "a cheerful", "a jolly", "a jovial", "a festive", "a lighthearted", "a carefree", "a zippy", "a zesty"],
+                ];
 
-function generateTable() {
-    var table = document.getElementById("table");
-};
-
-
+//creating a second noun array
+const noun2 = [
+                ["dog", "cat", "bird", "fish", "rabbit", "hamster", "turtle", "lizard", "snake", "frog", "toad", "salamander", "iguana", "chameleon", "gecko", "crocodile", "alligator", "dinosaur", "dragon", "unicorn", "pegasus", "phoenix", "mermaid"],
+                ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon", "mango", "nectarine", "orange", "pear", "raspberry", "strawberry", "tangerine", "watermelon", "zucchini"]
+              ];
